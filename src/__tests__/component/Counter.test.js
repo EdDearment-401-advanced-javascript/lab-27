12273d3e74bdeca1d4c6f1154184ea0822c2ec0c
ekearm <ed787.ed@gmail.com>
2019-07-02 21:50:24 -0700

    first test passed

---
+++ src/__tests__/component/Counter.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Counter from '../../components/Counter/Counter';
+
+describe('Counter component', () => {
+  it('has an initial state', () => {
+    // eslint-disable-next-line no-undef
+    const app = shallow(<Counter />);
+
+    expect(app.find('.count').text()).toBe('0');
+  });
+
+});
